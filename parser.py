7f2dbc0713406f62c5be34db65192b1213a25f81
Mateus <77507023+mateusriibeiro@users.noreply.github.com>
2022-01-10 19:35:03 -0300

    Added main

--- parser.py
+++ parser.py
@@ -4,11 +4,11 @@ import argparse, pereader
 arg = argparse.ArgumentParser()
 arg.add_argument("-f", dest="path", help="Complete path to file")
 args = arg.parse_args()
-
 pe = pereader.PE(args.path)
 
 def save_file():
-  with open ("parser.txt", "w") as f:
-    print(pe, file=f)
+    with open ("parser.txt", "w") as f:
+        print(pe, file=f)
 
-save_file()
+if __name__ == '__main__':
+    save_file()
